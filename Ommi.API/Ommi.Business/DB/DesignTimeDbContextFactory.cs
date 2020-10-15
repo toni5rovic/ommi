b1c78267d842d55c5a39bc7e6409091730921987
Antonije Petrovic <tpetrovic33@gmail.com>
2020-10-15 17:05:09 +0200

    Added necessary ContextFactory for Migrations

---
+++ Ommi.API/Ommi.Business/DB/DesignTimeDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Ommi.Business.DB
+{
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<OmmiDbContext>
+	{
+		public OmmiDbContext CreateDbContext(string connectionString)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<OmmiDbContext>();
+			optionsBuilder.UseSqlServer(connectionString);
+			return new OmmiDbContext(optionsBuilder.Options);
+		}
+
+		public OmmiDbContext CreateDbContext(string[] args)
+		{
+			//var jsonBytes = File.ReadAllBytes(@"..\Ommi.Web\appsettings.Development.json");
+			string jsonContent = File.ReadAllText(@"..\Ommi.Web\appsettings.Development.json");
+			var jsonDoc = JsonDocument.Parse(jsonContent);
+			var root = jsonDoc.RootElement;
+			string conString = root.GetProperty("ConnectionStrings").GetProperty("OmmiDbConnectionString").GetString();
+
+			return CreateDbContext(conString);
+		}
+	}
+}
