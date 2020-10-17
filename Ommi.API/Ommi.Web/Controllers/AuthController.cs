45ed6bdb0e02abbb170fb1f608e8882c62d474a0
Antonije Petrovic <tpetrovic33@gmail.com>
2020-10-17 19:56:43 +0200

    api: Added AuthController

---
+++ Ommi.API/Ommi.Web/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Ommi.Business.DTOs;
+using Ommi.Business.Services.Interfaces;
+using Ommi.Web.Models.Requests;
+using Ommi.Web.Models.Responses;
+
+namespace Ommi.Web.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AuthController : ControllerBase
+	{
+		private readonly IUserService userService;
+		private readonly IMapper autoMapper;
+		
+		public AuthController(IUserService userService, IMapper autoMapper)
+		{
+			this.userService = userService;
+			this.autoMapper = autoMapper;
+		}
+
+		// POST /auth/login
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LogInRequest logInRequest)
+		{
+			string token = await userService.LogInAsync(logInRequest.Username, logInRequest.Password);
+
+			var logInResponse = new LogInResponse() 
+			{ 
+				Token = token,
+				Username = logInRequest.Username
+			};
+
+			return Ok(logInResponse);
+		}
+
+		// POST /auth/register
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+		{
+			var registerModel = this.autoMapper.Map<UserDTO>(registerRequest);
+			var dtoRes = await userService.RegisterAsync(registerModel);
+			var response = this.autoMapper.Map<RegisterResponse>(dtoRes);
+			
+			return Ok(response);
+		}
+	}
+}
