ef9f0b5de6c8473170fc8769c2f7375e69ed398b
toni5rovic <toni.petrovic@elfak.rs>
2020-10-22 00:53:25 +0200

    client: Removed unnecessary CSS

--- ommi.client/src/components/drum-machine-components/StepSequencer.vue
+++ ommi.client/src/components/drum-machine-components/StepSequencer.vue
@@ -8,16 +8,9 @@
     <template
       v-for="(track, index) in boardState.tracks"
     >
-      <v-chip close label :key="track.soundName" class="ma-2 mr-1 pl-2 white--text" color="#705754" @click:close="remove(track)">
+      <v-chip close label :key="track.soundName" class="ma-2 mr-1 pl-2 white--text" color="#545C60" @click:close="remove(track)">
         {{ track.instrumentName }}
       </v-chip>
-      <!-- <v-btn
-        :key="track.soundName"
-        :style="`grid-column: 1; grid-row: ${index+1};`"
-        class="primary text-button ma-0 pt-1 pb-1"
-      >
-        {{ track.instrumentName }}
-      </v-btn> -->
       <step
         v-for="(step, stepIndex) in track.steps"
         :key="`${track.soundName}-${stepIndex}`"
@@ -57,15 +50,5 @@ export default {
   display: grid;
   grid-template-columns: 120px repeat(16, auto);
   margin: 0px 20px 20px;
-}
-.st-name {
-  background: linear-gradient(#292929, #111);
-  border: 1px solid #555;
-  color: white;
-  font-size: 14px;
-  margin: 0px;
-  vertical-align: middle;
-  padding: 0px 10px;
-  line-height: 50px;
 }
 </style>
