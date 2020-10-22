a2d9376fafa612e9f3fdac99f51883de38553a49
toni5rovic <toni.petrovic@elfak.rs>
2020-10-22 09:42:23 +0200

    client: Added instrument icons

--- ommi.client/src/components/add-track-modal.vue
+++ ommi.client/src/components/add-track-modal.vue
@@ -18,9 +18,12 @@
               v-for="(item, itemIndex) in instruments"
               :key="itemIndex"
             >
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
+              <v-list-item-avatar size="40">
+                <!-- <v-img max-height="80%" contain :src="item.icon"></v-img> -->
+                <!-- <v-img :src="item.icon" max-height="25px" /> -->
+                <img v-bind:src="getImgUrl(item.icon)" >
+                <!-- <img src="../assets/bass-guitar.svg" > -->
+              </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title v-text="item.instrumentName"></v-list-item-title>
               </v-list-item-content>
@@ -36,6 +39,7 @@
         <v-btn color="primary" @click="addSelectedTrack">OK</v-btn>
         <v-btn color="primary" @click="show=false">Close</v-btn>
       </v-card-actions>
+      <v-alert type="error" v-if="trackAlreadyExistsError">Selected track already exists. Please choose another one.</v-alert>
     </v-card>
   </v-dialog>
 </template>
@@ -48,16 +52,17 @@ export default {
     addTrackCallback: Function
   },
   data: () => ({
+    trackAlreadyExistsError: false,
     selectedItem: 1,
     instruments: [
-      { instrumentName: 'Bass', soundName: 'bass', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'Clap', soundName: 'clap', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'HiHat 1', soundName: 'hihat', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'HiHat 2', soundName: 'hat2', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'Kick', soundName: 'kick', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'Open HiHat', soundName: 'openhihat', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'Snare', soundName: 'snare', icon: 'mdi-guitar-acoustic' },
-      { instrumentName: 'Sub', soundName: 'sub', icon: 'mdi-guitar-acoustic' }
+      { instrumentName: 'Bass', soundName: 'bass', icon: 'bass-guitar.svg' },
+      { instrumentName: 'Clap', soundName: 'clap', icon: 'clapping.svg' },
+      { instrumentName: 'HiHat 1', soundName: 'hihat', icon: 'hi-hat.svg' },
+      { instrumentName: 'HiHat 2', soundName: 'hat2', icon: 'hi-hat.svg' },
+      { instrumentName: 'Kick', soundName: 'kick', icon: 'karate.svg' },
+      { instrumentName: 'Open HiHat', soundName: 'openhihat', icon: 'open-hi-hat.svg' },
+      { instrumentName: 'Snare', soundName: 'snare', icon: 'snare.svg' },
+      { instrumentName: 'Sub', soundName: 'sub', icon: 'tom.svg' }
     ]
   }),
   computed: {
@@ -71,10 +76,37 @@ export default {
     }
   },
   methods: {
+    getImgUrl (imgName) {
+      return require('../assets/' + imgName)
+    },
     addSelectedTrack () {
       const selectedTrack = this.instruments[this.selectedItem]
+      if (this.trackAlreadyExists(selectedTrack.soundName)) {
+        this.showTrackAlreadyExistsError()
+        return
+      }
+
       this.addTrackCallback(selectedTrack.soundName, selectedTrack.instrumentName)
       this.show = false
+    },
+
+    trackAlreadyExists (soundName) {
+      for (let i = 0; i < this.$store.state.boardState.tracks.length; i++) {
+        const track = this.$store.state.boardState.tracks[i]
+        if (track.soundName === soundName) {
+          return true
+        }
+      }
+
+      return false
+    },
+
+    showTrackAlreadyExistsError () {
+      this.trackAlreadyExistsError = true
+
+      setTimeout(() => {
+        this.trackAlreadyExistsError = false
+      }, 3000)
     }
   }
 }
